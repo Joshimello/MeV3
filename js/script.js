e62277a70891d0b1e932a722e6b3d48cfaaee39a
Joshimello <joshualeanjw@gmail.com>
2022-07-23 23:05:53 +0800

    js cleanup

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
-// title text rotation
-
 $(window).on('load', () => {
+
+	// entry animation
+	for (const [key, value] of Object.entries({ '.a': '0', '.b': '500', '.c': '1000' })) {
+		ScrollReveal().reveal('.title ' + key, {
+			distance: '0.2rem',
+			duration: '1000',
+			scale: 0.75,
+			delay: value
+		})
+	}
+
+	for (const [key, value] of Object.entries({ 'h3': '1', 'h6': '0', 'img': '0' })) {
+		ScrollReveal().reveal('.title ' + key, {
+			distance: '0.5rem',
+			duration: '1000',
+			opacity: value,
+			delay: '2000',
+		})
+	}
+
+	// title text rotation
 	$('#title-text').teletype({
 	    text: [
 		    'Web Development',
@@ -12,45 +31,6 @@ $(window).on('load', () => {
 	    typeDelay: -50,
 	    backDelay: -50,
 	   	delay: 1000
-	})  
-
-	ScrollReveal().reveal('.title .a', {
-		distance: '0.2rem',
-		duration: '1000',
-		scale: 0.75
-	})
-
-	ScrollReveal().reveal('.title .b', {
-		distance: '0.2rem',
-		duration: '1000',
-		scale: 0.75,
-		delay: '500',
-	})
-
-	ScrollReveal().reveal('.title .c', {
-		distance: '0.2rem',
-		duration: '1000',
-		scale: 0.75,
-		delay: '1000',
-	})
-
-	ScrollReveal().reveal('.title h3', {
-		distance: '0.5rem',
-		duration: '1000',
-		opacity: 1,
-		delay: '2000',
-	})
-
-	ScrollReveal().reveal('.title h6', {
-		distance: '0.5rem',
-		duration: '1000',
-		delay: '2000'
-	})
-
-	ScrollReveal().reveal('.title img', {
-		distance: '0.5rem',
-		duration: '1000',
-		delay: '2000'
 	})
 
 })
